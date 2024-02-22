e26b2a35e955e28bea131ce1d91abd8b524d0161
hyub lee <hyublee@hyubui-MacBookPro.local>
2024-02-23 00:56:52 +0900

    cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import crossOriginIsolation from "vite-plugin-cross-origin-isolation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
